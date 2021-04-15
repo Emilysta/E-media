fe31b90eda2cadc4f47cf08c413b8f8e6c9b7f18
Michał Kaleta <mkaleta1999@gmail.com>
2021-04-15 11:17:36 +0200

    Dodane parsowanie xml'a

--- E-media/MetaTab.xaml.cs
+++ E-media/MetaTab.xaml.cs
@@ -7,23 +7,34 @@ using Windows.Storage.Streams;
 using Windows.Storage;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using System.Text.RegularExpressions;
 
 namespace E_media
 {
-    
+
     public class XMLPair
     {
         public string PropertyName { get; set; }
         public string Value { get; set; }
     }
+    public class XMLNode
+    {
+        public string Name { get; set; }
+        public List<XMLPair> Attributes { get; set; }
+        public List<XMLNode> Children { get; set; }
+        public XMLNode Parent { get; set; }
+        public string Content { get; set; }
+    }
 
     public sealed partial class MetaTab : Page
     {
 
-        List<string> markerLikeSVG = new List<string>() { "<svg", "<?xml","<!DOCTYPE","<!--" };
-        List<string> markerLikeTitle = new List<string>() { "title", "metadata", "desc"};
+        List<string> markerWithParameters = new List<string>() { "<svg", "<?xml", "<!DOCTYPE" };
+        List<string> markerWithContent = new List<string>() { "title", "metadata", "desc", "<!--" };
         public int myID;
         public List<XMLPair> Metadata { get; set; }
+
+        List<XMLNode> Nodes { get; set; }
         public MetaTab()
         {
             this.InitializeComponent();
@@ -37,7 +48,7 @@ namespace E_media
             s[1] = s[1].TrimStart('"');
             s[1] = s[1].TrimEnd('"');
             pair.Value = s[1];
-            Debug.WriteLine(pair.PropertyName + "=" + pair.Value);
+            //Debug.WriteLine(pair.PropertyName + "=" + pair.Value);
             return pair;
         }
 
@@ -52,7 +63,7 @@ namespace E_media
                 while ((tempLine = reader.ReadLine()) != null)
                 {
                     tempLine = tempLine.Trim();
-                    if(tempLine.StartsWith("<"))
+                    if (tempLine.StartsWith("<"))
                     {
                         line = tempLine;
                     }
@@ -61,23 +72,23 @@ namespace E_media
                         line += " ";
                         line += tempLine;
                     }
-                    if(line.EndsWith(">"))
+                    if (line.EndsWith(">"))
                     {
                         int j = 0;
-                        for(int i =0;i<markerLikeSVG.Count;i++)
+                        for (int i = 0; i < markerWithParameters.Count; i++)
                         {
-                            if(line.StartsWith(markerLikeSVG[i]))
+                            if (line.StartsWith(markerWithParameters[i]))
                             {
                                 ReadMarkerLikeSVG(line);
-                                j = markerLikeTitle.Count;
+                                j = markerWithContent.Count;
                                 break;
                             }
                         }
-                        for (int i = j; i < markerLikeTitle.Count; i++)
+                        for (int i = j; i < markerWithContent.Count; i++)
                         {
-                            if (line.StartsWith("<"+markerLikeTitle[i]+">") && line.EndsWith("</"+markerLikeTitle[i]+">")) 
+                            if (line.StartsWith("<" + markerWithContent[i] + ">") && line.EndsWith("</" + markerWithContent[i] + ">"))
                             {
-                                ReadMarkerLikeTitle(line,markerLikeTitle[i]);
+                                ReadMarkerLikeTitle(line, markerWithContent[i]);
                                 break;
                             }
                         }
@@ -85,16 +96,17 @@ namespace E_media
                     }
                 }
             }
-            metaList.ItemsSource = Metadata; 
+            metaList.ItemsSource = Metadata;
         }
 
-        private void ReadMarkerLikeTitle(string line,string marker)
+        private void ReadMarkerLikeTitle(string line, string marker)
         {
-            line=line.Replace("<"+marker+">","");
-            line=line.Replace("</" + marker + ">", "");
-            Metadata.Add(new XMLPair() {
-            PropertyName = marker,
-            Value = line
+            line = line.Replace("<" + marker + ">", "");
+            line = line.Replace("</" + marker + ">", "");
+            Metadata.Add(new XMLPair()
+            {
+                PropertyName = marker,
+                Value = line
             });
         }
 
@@ -103,12 +115,12 @@ namespace E_media
             string[] pairs = line.Split();
             int lastIndex = pairs.Count() - 1;
             pairs[lastIndex] = pairs[lastIndex].Trim('>');
-            if(line.StartsWith("<?xml"))
+            if (line.StartsWith("<?xml"))
             {
                 pairs[lastIndex] = pairs[lastIndex].Trim('?');
             }
 
-            for (int i = 0;i<lastIndex+1;i++)
+            for (int i = 0; i < lastIndex + 1; i++)
             {
                 string temp = pairs[i];
                 if (temp.Contains('='))
@@ -120,7 +132,7 @@ namespace E_media
                     else
                     {
                         string temp2 = temp;
-                        while(!temp2.EndsWith('"'))
+                        while (!temp2.EndsWith('"'))
                         {
                             ++i;
                             temp2 += " ";
@@ -132,10 +144,103 @@ namespace E_media
             }
         }
 
+        private async void ReadMetadata2()
+        {
+            Nodes = new List<XMLNode>();
+            string content;
+            IRandomAccessStream stream = await ImageListClass.FileSourceList[myID].OpenAsync(FileAccessMode.Read);
+            using (StreamReader reader = new StreamReader(stream.AsStreamForRead()))
+            {
+                content = reader.ReadToEnd();
+            }
+            content = content.Replace("\n", "").Replace("\r", "").Replace("\t", "");
+            string pattern = "(?<=[>])";
+            string[] lines = Regex.Split(content, pattern);
+
+            XMLNode currentNode = null;
+            XMLNode currentParent = null;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Trim();
+                if (lines[i].StartsWith("</")) //Pomin linijki zamykajace tag
+                {
+                    currentNode = currentParent;
+                    if (currentParent != null)
+                        currentParent = currentParent.Parent;
+                    else
+                        currentParent = null;
+                    continue;
+                }
+
+                if (lines[i].StartsWith('<')) //Otwarcie tagu
+                {
+                    XMLNode node = ReadNode(lines[i]);
+                    Nodes.Add(node);
+                    currentParent = currentNode;
+                    currentNode = Nodes.Last();
+                    if (currentParent != null)
+                        currentParent.Children.Add(currentNode);
+                    currentNode.Parent = currentParent;
+                }
+                else
+                {
+                    string tmp = lines[i].Split("</")[0];
+                    Nodes.Last().Content = tmp;
+                    if (lines[i].Contains("</"))
+                    {
+                        currentNode = currentParent;
+                        if (currentParent != null)
+                            currentParent = currentParent.Parent;
+                        else
+                            currentParent = null;
+                    }
+                }
+
+                if (lines[i].EndsWith("/>") || lines[i].EndsWith("?>"))
+                {
+                    currentNode = currentParent;
+                    if (currentParent != null)
+                        currentParent = currentParent.Parent;
+                    else
+                        currentParent = null;
+                }
+
+            }
+        }
+        private XMLNode ReadNode(string line)
+        {
+            string[] words = line.Split();
+            string temp = line.TrimStart('<').TrimStart('/').TrimEnd('>').TrimEnd('/').Split()[0];
+            XMLNode node = new XMLNode();
+            node.Name = temp;
+            node.Attributes = new List<XMLPair>();
+            node.Children = new List<XMLNode>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains("="))
+                {
+                    string s = words[i].TrimEnd('>').TrimEnd('/').TrimEnd('?');
+                    if (!s.EndsWith('"'))
+                    {
+                        int j = 1;
+                        while (!s.EndsWith('"'))
+                        {
+                            s += " " + words[i + j].TrimEnd('>').TrimEnd('/').TrimEnd('?');
+                            j++;
+                        }
+                    }
+                    node.Attributes.Add(ReadXMLPair(s));
+                }
+            }
+            return node;
+        }
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             myID = (int)e.Parameter;
-            ReadMetadata();
+            ReadMetadata2();
         }
     }
 }
